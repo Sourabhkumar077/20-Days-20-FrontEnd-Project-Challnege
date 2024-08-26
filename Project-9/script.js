8cc9878d480d95a6a3860be65a7cbf7aa2765eff
sourabh kumar <147600346+Sourabhkumar077@users.noreply.github.com>
2024-08-26 16:39:37 +0530

    some modifications

--- Project-9/script.js
+++ Project-9/script.js
@@ -2,23 +2,6 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
-function showNotes(){
-    if (localStorage.getItem("notes")) {
-        notesContainer.innerHTML = localStorage.getItem("notes");
-    }
-}
-showNotes();
-
-// function to add local browser storage
-function updateStorage(){
-    let notesHtml = "";
-    notes = document.querySelectorAll(".input-box");
-    notes.forEach(note => {
-        notesHtml += `<p class="input-box" contenteditable="true">${note.textContent}</p>`;
-    });
-    localStorage.setItem("notes", notesHtml);
-}
-
 // function to add input-box dynamically
 createBtn.addEventListener('click', () => {
     let inputBox = document.createElement("p");
@@ -27,26 +10,22 @@ createBtn.addEventListener('click', () => {
     inputBox.setAttribute("contenteditable", "true");
     img.src = "./images/delete.png";
     notesContainer.appendChild(inputBox).appendChild(img);
-    updateStorage();
 });
-
 
 notesContainer.addEventListener('click', function(e) {
     if (e.target.tagName === "IMG") {
         e.target.parentElement.remove();
-        updateStorage();
     }
     //for retain the data check this conditon
     else if(e.target.tagName === "p"){
         notes = document.querySelectorAll(".input-box");
         notes.forEach(nt => {
             nt.onkeyup = function(){
-                updateStorage();
+                // do nothing
             }
         });
     }
 });
-
 
 document.addEventListener('keydown',event =>{
     if(event.key === "Enter"){
